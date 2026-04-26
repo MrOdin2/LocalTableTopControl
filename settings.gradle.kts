bc68a5a687e16ae295448c836fe4cfe9fffaf02a
MrOdin <fabian.klimkeit001@stud.fh-dortmund.de>
2026-04-26 22:22:55 +0200

    rename folder to fit module name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.name = "tabletopcontrol"
 
 include("core", "map", "audio", "light", "dynamicmap_builder")
-project(":dynamicmap_builder").projectDir = file("dynamicmap")
+project(":dynamicmap_builder").projectDir = file("dynamicmap_builder")
 
 include("tracker")
