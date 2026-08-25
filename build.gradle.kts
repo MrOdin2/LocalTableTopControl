15249d85c4506407937ca66a63d6058e187a68da
MrOdin <fabian.klimkeit001@stud.fh-dortmund.de>
2026-08-25 19:01:49 +0200

    change version number to 1.5.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 allprojects {
     group = "com.tabletopcontrol"
-    version = "1.0.0" // THIS IS OUR VERSION NUMBER
+    version = "1.5.0" // THIS IS OUR VERSION NUMBER
 
     repositories {
         mavenCentral()
